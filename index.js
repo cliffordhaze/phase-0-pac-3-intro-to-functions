f6883c9c78e375486ea3daaaf2e5838d68d8fe72
Nicole Gomez <nicolegomezx3@gmail.com>
2023-03-02 11:34:39 -0500

    Adds sayHelloTo

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 function sayHelloToGuadalupe() {
     console.log("Hello, Guadalupe!");
 }
-sayHelloToGuadalupe();
 
 function sayHelloToLiz() {
     console.log("Hello, Liz!");
 }
-sayHelloToLiz();
 
 function sayHelloToSamip() {
     console.log("Hello, Samip!");
 }
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
 sayHelloToSamip();
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+
+sayHelloTo("Nicole");
+sayHelloTo("Jane");
+sayHelloTo("R2-D2");
+sayHelloTo(1);
